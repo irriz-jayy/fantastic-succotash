08ff5b7b3237756bf27bc997e6370ef0e114465a
irriz-jayy <jayfelix789@gmail.com>
2023-11-08 12:52:14 +0300

    added responsiveness

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,14 +4,14 @@ import owner from "../assets/hero-woman.png";
 const About = () => {
   return (
     <>
-      <div className="h-[80vh] bg-secondary">
+      <div className="min-h-[80vh] bg-secondary">
         {/* header section */}
         <header className="p-2 text-4xl text-center font-main text-primary">
           About Me
         </header>
         {/* about paragraph */}
-        <div className="flex justify-between">
-          <p className="flex items-center w-1/2 p-2 m-auto text-lg border text-primary font-text h-96">
+        <div className="flex flex-col justify-between md:flex-row lg:flex-row">
+          <p className="flex items-center p-2 m-auto text-lg border md:w-1/2 lg:w-1/2 text-primary font-text md:h-96 lg:h-96">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quaerat
             in voluptatibus eligendi, vero placeat magnam eveniet iste
             consequatur at, quibusdam, numquam voluptatum eius amet. Ipsa
@@ -20,6 +20,7 @@ const About = () => {
             vero placeat magnam eveniet iste consequatur at, quibusdam, numquam
             voluptatum eius amet. Ipsa corporis corrupti consequatur nemo.
           </p>
+          {/* image */}
           <img src={owner} alt="" className="m-auto border" />
         </div>
       </div>
