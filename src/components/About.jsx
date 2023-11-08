be3e59201c2b8235e5b2e84f711b8c72e54c5b91
irriz-jayy <jayfelix789@gmail.com>
2023-11-08 12:49:00 +0300

    styled for lg screens the about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import owner from "../assets/hero-woman.png";
 
 const About = () => {
   return (
     <>
-      <div className="h-[100vh]">About</div>
+      <div className="h-[80vh] bg-secondary">
+        {/* header section */}
+        <header className="p-2 text-4xl text-center font-main text-primary">
+          About Me
+        </header>
+        {/* about paragraph */}
+        <div className="flex justify-between">
+          <p className="flex items-center w-1/2 p-2 m-auto text-lg border text-primary font-text h-96">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quaerat
+            in voluptatibus eligendi, vero placeat magnam eveniet iste
+            consequatur at, quibusdam, numquam voluptatum eius amet. Ipsa
+            corporis corrupti consequatur nemo.Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Eos quaerat in voluptatibus eligendi,
+            vero placeat magnam eveniet iste consequatur at, quibusdam, numquam
+            voluptatum eius amet. Ipsa corporis corrupti consequatur nemo.
+          </p>
+          <img src={owner} alt="" className="m-auto border" />
+        </div>
+      </div>
     </>
   );
 };
